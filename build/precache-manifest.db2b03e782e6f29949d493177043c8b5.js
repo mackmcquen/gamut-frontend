7b0fea00c24ece012ea9e1c36c6d9fbafd7e550c
mackmcquen <mackmcquen@comcast.net>
2020-08-26 02:51:16 -0500

    redirects for netlify

---
+++ build/precache-manifest.db2b03e782e6f29949d493177043c8b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72922b062cf2f8ff6d4f3322d5a16e88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bebd87010877123a016d",
+    "url": "/static/css/main.3b019f9c.chunk.css"
+  },
+  {
+    "revision": "c1d8e8dc88764cbe6e02",
+    "url": "/static/js/2.1dac7871.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.1dac7871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bebd87010877123a016d",
+    "url": "/static/js/main.ebdbe864.chunk.js"
+  },
+  {
+    "revision": "de83fd8d684701878347",
+    "url": "/static/js/runtime-main.33c7c54f.js"
+  }
+]);
